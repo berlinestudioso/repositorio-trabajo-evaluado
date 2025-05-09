9285e5f928cfa45ed3e46b63caedef60c9c03c13
berlinestudioso <elberlinarenoso@gmail.com>
2025-05-09 16:06:19 -0400

    Agregado Informe Latex solicitado en .tex y .pdf

---
+++ cumpleaños.py
@@ -0,0 +1,7 @@
+print("Calculadora de cumpleaños")
+dia = int(input("Día de tu nacimiento: "))
+mes = int(input("Mes de tu nacimiento: "))
+año = int(input("Año de tu nacimiento: "))
+
+print(f"Naciste el {dia}/{mes}/{año}")
+print("Sigueme para más códigos de alto nivel")
